5bdbbf52c9b21432f6da553588ee70914ddac58e
Jonathan <theflametrooper@gmail.com>
2019-09-09 23:08:42 +0100

    fix: bluebird isn't being used
    Signed-off-by: Jonathan <theflametrooper@gmail.com>

--- lofig.js
+++ lofig.js
@@ -21,13 +21,12 @@ window.lofig = {
 			.then(response => {
 				return response.json();
 			}).then(json => {
-				query = query.split('.');
-				return Promise.each(query, property => {
-					json = json[property];
-				}).then(() => {
-					if (json) return true;
-					else return false;
-				});
+				query.split('.')
+					.forEach(property => {
+						json = json[property];
+					});
+
+				return json;
 			}).catch(err => {
 				console.log('parsing failed', err);
 			});
